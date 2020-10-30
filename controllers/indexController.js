4bcb8fc14ebe98ebcfba0c7103898c98bb8652c9
Amélie Salaün <ameliesalaun@MacBook-Pro.local>
2020-10-30 09:39:35 +0400

    Add displayValue on screen

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -10,26 +10,4 @@ controller.indexView = async (req, res, next) => {
   }
 }
 
-
-/*
-  Algo simple calculatrice
-
-  -L'utilisateur clique sur un chiffre
-  - Le programme récupère ce chiffre
-
-  - L'utilisateur clique sur un opérateur (+, -, x, /)
-
-  - L'utilisateur clique sur un deuxieme chiffre
-  - Le programme récupère ce chiffre
-
-  - Le programme effectue le calcul grâce à l'opérateur
-
-  - L'utilisateur clique sur =
-
-  - Le programme donne le résultat du calcul sur l'écran de la calculatrice
-  - Le programme stocke le résultat du calcul dans local storage (10 derniers resultats)
-
-
-*/
-
 module.exports = controller;
